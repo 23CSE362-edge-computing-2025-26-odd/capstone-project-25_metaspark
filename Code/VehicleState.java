caa7b36f1fa9b0024dc8a0c674abc1f5079e2bf5
Kanishkhan <117373902+Kanishkhan@users.noreply.github.com>
2025-09-19 21:53:05 +0530

    Updated VehicleState.java
    Fixed CPU storage

--- Code/VehicleState.java
+++ Code/VehicleState.java
@@ -1,6 +1,9 @@
 package org.fog.marina;
 
 public class VehicleState {
+    private static final double DEFAULT_CPU = 50.0; 
+    private static final double DEFAULT_STORAGE = 100.0;
+
     private String id;
     private int time;
     private double x, y;
@@ -9,26 +12,22 @@ public class VehicleState {
     private double storage;
     private boolean available;
 
+    private double vx, vy;
 
-    private double vx, vy; 
+    private double predictedX, predictedY, predictedSpeed;
 
-
-    private double predictedX, predictedY, predictedSpeed, predictedCpuCapacity;
-
-    public VehicleState(int time, String id, double x, double y,
-                        double speed, double cpuCapacity, double storage) {
+    public VehicleState(int time, String id, double x, double y, double speed) {
         this.time = time;
         this.id = id;
         this.x = x; this.y = y;
         this.speed = speed;
-        this.cpuCapacity = cpuCapacity;
-        this.storage = storage;
+        this.cpuCapacity = DEFAULT_CPU;
+        this.storage = DEFAULT_STORAGE;
         this.available = true;
 
         this.predictedX = x;
         this.predictedY = y;
         this.predictedSpeed = speed;
-        this.predictedCpuCapacity = cpuCapacity;
 
         double angle = Math.random() * 2 * Math.PI;
         this.vx = speed * Math.cos(angle);
@@ -50,21 +49,16 @@ public class VehicleState {
     public boolean isAvailable() { return available; }
 
     public void setAvailable(boolean available) { this.available = available; }
-    public void setCpuCapacity(double cpuCapacity) { this.cpuCapacity = cpuCapacity; }
 
-    public void updatePrediction(double predictedX, double predictedY,
-                                 double predictedSpeed, double predictedCpuCapacity) {
+    public void updatePrediction(double predictedX, double predictedY, double predictedSpeed) {
         this.predictedX = predictedX;
         this.predictedY = predictedY;
         this.predictedSpeed = predictedSpeed;
-        this.predictedCpuCapacity = predictedCpuCapacity;
-        this.cpuCapacity = predictedCpuCapacity;
     }
 
     public double getPredictedX() { return predictedX; }
     public double getPredictedY() { return predictedY; }
     public double getPredictedSpeed() { return predictedSpeed; }
-    public double getPredictedCpuCapacity() { return predictedCpuCapacity; }
 
     public boolean canProcess(Task task) {
         return available && task.getCpu() <= cpuCapacity && task.getSize() <= storage;
@@ -76,11 +70,10 @@ public class VehicleState {
         double finishTime = currentTime + procTime;
         task.setFinishTime(finishTime);
 
-        double cost = task.getCpu() * 1.0;
+        double cost = task.getCpu() * 1.0; 
         task.setCost(cost);
 
         System.out.println("Task " + task.getId() + " assigned to Vehicle " + id
-                           + " at time " + time
                            + " [Finish=" + finishTime + ", Deadline=" + (task.getArrivalTime() + task.getDeadline())
                            + ", Cost=" + cost + "]");
     }

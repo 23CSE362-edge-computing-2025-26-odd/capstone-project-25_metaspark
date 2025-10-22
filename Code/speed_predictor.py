f5d18bec895ceef94e3cda00ef177b921e2ed19b
Sakthi1217 <sakthikarthikbc@gmail.com>
2025-10-22 20:36:38 +0530

    Updated speed_predictor.py

--- Code/speed_predictor.py
+++ Code/speed_predictor.py
@@ -1,3 +1,4 @@
+# speed_predictor.py
 import numpy as np
 import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
@@ -7,68 +8,38 @@ from tensorflow.keras.layers import LSTM, Dense, Dropout
 class SpeedPredictor:
     def __init__(self, time_steps):
         self.time_steps = time_steps
-        self.num_features = 1  # Only 'speed'
-        self.scaler = MinMaxScaler(feature_range=(0, 1))
-        self.model = self._create_lstm_model()
+        self.num_features = 1
+        self.scaler = MinMaxScaler(feature_range=(0,1))
+        self.model = self._create_model()
         self.is_trained = False
 
-    def _create_lstm_model(self):
+    def _create_model(self):
         model = Sequential()
-        model.add(LSTM(units=128, input_shape=(self.time_steps, self.num_features), return_sequences=True))
+        model.add(LSTM(128, input_shape=(self.time_steps, self.num_features), return_sequences=True))
         model.add(Dropout(0.2))
-        model.add(LSTM(units=64))
+        model.add(LSTM(64))
         model.add(Dropout(0.2))
-        model.add(Dense(units=self.num_features, activation='linear'))
+        model.add(Dense(self.num_features, activation='linear'))
         model.compile(optimizer='adam', loss='mean_squared_error')
         return model
 
-    def train(self, data, epochs=15, batch_size=1, val_steps=5, verbose=1):
+    def train(self, data, epochs=10, batch_size=8, val_steps=5):
         T = len(data)
         if T <= self.time_steps + val_steps:
             return False
+        train_upto = T - val_steps
+        self.scaler.fit(data[:train_upto])
+        scaled = self.scaler.transform(data)
 
-        train_fit_upto = T - val_steps
-        self.scaler.fit(data[:train_fit_upto])
-        scaled_data = self.scaler.transform(data)
-
-        X_all, y_all = self._create_dataset(scaled_data)
-        y_all = y_all.reshape(y_all.shape[0], self.num_features)
-
-        if len(X_all) <= val_steps:
+        X, y = self._create_dataset(scaled)
+        if len(X) <= val_steps:
             return False
 
-        X_train, y_train = X_all[:-val_steps], y_all[:-val_steps]
-        X_val, y_val = X_all[-val_steps:], y_all[-val_steps:]
+        X_train, y_train = X[:-val_steps], y[:-val_steps]
+        X_val, y_val = X[-val_steps:], y[-val_steps:]
 
-        train_data = data[:train_fit_upto]
-        feature_range = np.maximum(train_data.max(axis=0) - train_data.min(axis=0), 1e-9)
-
-        class ValPctAccCallback(tf.keras.callbacks.Callback):
-            def __init__(self, X_val, y_val, scaler, feature_range):
-                super().__init__()
-                self.X_val = X_val
-                self.y_val = y_val
-                self.scaler = scaler
-                self.feature_range = feature_range
-
-            def on_epoch_end(self, epoch, logs=None):
-                y_pred_scaled = self.model.predict(self.X_val, verbose=0)
-                y_pred = self.scaler.inverse_transform(y_pred_scaled)
-                y_true = self.scaler.inverse_transform(self.y_val)
-                mae_per_feature = np.mean(np.abs(y_pred - y_true), axis=0)
-                nmae = float(np.mean(mae_per_feature / self.feature_range))
-                acc_pct = max(0.0, 100.0 * (1.0 - nmae))
-                print(f"{acc_pct:.2f}%")
-
-        self.model.fit(
-            X_train,
-            y_train,
-            validation_data=(X_val, y_val),
-            epochs=epochs,
-            batch_size=batch_size,
-            verbose=0,
-            callbacks=[ValPctAccCallback(X_val, y_val, self.scaler, feature_range)],
-        )
+        self.model.fit(X_train, y_train, validation_data=(X_val, y_val),
+                       epochs=epochs, batch_size=batch_size, verbose=0)
         self.is_trained = True
         return True
 
@@ -80,15 +51,12 @@ class SpeedPredictor:
         return np.array(X), np.array(y)
 
     def predict_next(self, last_known_data, num_predictions=1):
-        if not self.is_trained:
-            return []
-        predictions = []
-        current_input_sequence = self.scaler.transform(last_known_data).reshape(1, self.time_steps, self.num_features)
+        if not self.is_trained: return []
+        seq = self.scaler.transform(last_known_data).reshape(1, self.time_steps, self.num_features)
+        preds = []
         for _ in range(num_predictions):
-            predicted_scaled = self.model.predict(current_input_sequence, verbose=0)
-            predicted_values = self.scaler.inverse_transform(predicted_scaled)
-            predictions.append(predicted_values[0][0])  
-            current_input_sequence = np.vstack(
-                [current_input_sequence[0][1:], predicted_scaled[0].reshape(1, -1)]
-            ).reshape(1, self.time_steps, self.num_features)
-        return predictions
+            p_scaled = self.model.predict(seq, verbose=0)
+            p = self.scaler.inverse_transform(p_scaled)
+            preds.append(p[0][0])
+            seq = np.vstack([seq[0][1:], p_scaled[0].reshape(1, -1)]).reshape(1, self.time_steps, self.num_features)
+        return preds

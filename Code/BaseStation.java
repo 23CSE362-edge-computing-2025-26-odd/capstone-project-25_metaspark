994d9d99c528fde801f13a72dcc52cea26ece88c
KARTHIK P <itskp2004@gmail.com>
2025-10-22 19:29:57 +0530

    Update BaseStation.java
    modifications has been made to adapt to dynamic environment

--- Code/BaseStation.java
+++ Code/BaseStation.java
@@ -1,44 +1,83 @@
 package org.fog.marina;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class BaseStation {
     private String id;
     private double x, y;
-    private double cpuCapacity;
-    private double storage;
     private double range;
+    private double cpuCapacity;
+    private double usedCpu;
+    private double storage;
+    private List<Task> runningTasks;
 
-    private static final double COST_FACTOR = 11.444;
-
-    public BaseStation(String id, double x, double y, double cpuCapacity, double storage, double range) {
+    public BaseStation(String id, double x, double y, double range, double cpuCapacity, double storage) {
         this.id = id;
         this.x = x;
         this.y = y;
-        this.cpuCapacity = cpuCapacity; 
-        this.storage = storage;
         this.range = range;
+        this.cpuCapacity = cpuCapacity;
+        this.usedCpu = 0.0;
+        this.storage = storage;
+        this.runningTasks = new ArrayList<>();
     }
 
     public String getId() { return id; }
-    public double getCpuCapacity() { return cpuCapacity; }
-    public double getStorage() { return storage; }
     public double getX() { return x; }
     public double getY() { return y; }
     public double getRange() { return range; }
+    public double getCpuCapacity() { return cpuCapacity; }
+    public double getUsedCpu() { return usedCpu; }
+    public double getAvailableCpu() { return Math.max(0.0, cpuCapacity - usedCpu); }
+    public double getStorage() { return storage; }
 
-    public boolean canProcess(Task task) {
-        return task.getCpu() <= cpuCapacity && task.getSize() <= storage;
+    public boolean canProcess(Task t) {
+        return (t.getCpu() <= getAvailableCpu()) && (t.getSize() <= storage);
     }
 
-    public void assignTask(Task task, double currentTime) {
-        double procTime = task.computeProcessingTime(cpuCapacity);
+    public void assignTask(Task t, double currentTime) {
+        double procTime = t.computeProcessingTime(cpuCapacity);
         double finishTime = currentTime + procTime;
-        task.setFinishTime(finishTime);
+        t.setFinishTime(finishTime);
+        usedCpu += t.getCpu();
+        runningTasks.add(t);
+        double cost = t.getCpu() * 11.444;
+        t.setCost(cost);
+        System.out.println(t.getId() +
+                " -> BaseStation " + id +
+                " [Finish=" + String.format("%.3f", finishTime) +
+                ", Deadline=" + (t.getArrivalTime() + t.getDeadline()) +
+                ", Cost=" + String.format("%.3f", cost) +
+                ", BS_usedCpu=" + String.format("%.3f", usedCpu) + "]");
+    }
 
-        double cost = task.getCpu() * COST_FACTOR;
-        task.setCost(cost);
+    public void releaseTask(Task t) {
+        usedCpu = Math.max(0.0, usedCpu - t.getCpu());
+        runningTasks.remove(t);
+    }
 
-        System.out.println("Task " + task.getId() + " assigned to Base Station " + id +
-                           " [Finish=" + finishTime + ", Deadline=" + (task.getArrivalTime() + task.getDeadline()) +
-                           ", Cost=" + cost + "]");
+    public void releaseFinished(double currentTime) {
+        Iterator<Task> it = runningTasks.iterator();
+        while (it.hasNext()) {
+            Task t = it.next();
+            if (t.getFinishTime() <= currentTime) {
+                usedCpu = Math.max(0.0, usedCpu - t.getCpu());
+                it.remove();
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "BaseStation{" +
+                "id='" + id + '\'' +
+                ", CPU=" + usedCpu + "/" + cpuCapacity +
+                ", Storage=" + storage +
+                ", Range=" + range +
+                ", RunningTasks=" + runningTasks.size() +
+                '}';
     }
 }
+
